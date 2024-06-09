f6ca94a6dd5eb614528f3b66dd3ab185ada9ad71
Jangchaeyun <zzangsally@naver.com>
2024-06-09 21:36:11 +0900

    0609 ApplicationConfiguration

---
+++ src/main/java/com/day/DayTaskManagementApplication.java
@@ -0,0 +1,13 @@
+package com.day;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayTaskManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayTaskManagementApplication.class, args);
+	}
+
+}
