658678a1e6bf21bfb764993f298fd7e2800ed47e
Jangchaeyun <zzangsally@naver.com>
2024-06-16 22:49:37 +0900

    0618 TaskController

--- Task Service/src/main/java/com/day/controller/TaskController.java
+++ Task Service/src/main/java/com/day/controller/TaskController.java
@@ -1,13 +1,16 @@
 package com.day.controller;
 
 import com.day.model.Task;
+import com.day.model.TaskStatus;
+import com.day.model.UserDto;
 import com.day.service.TaskService;
+import com.day.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/tasks")
@@ -15,8 +18,66 @@ public class TaskController {
     @Autowired
     private TaskService taskService;
 
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
     public ResponseEntity<Task> createTask(@RequestBody Task task,
-                                           @RequestHeader("Authorization") String jwt) {
-        Task createdTask = taskService.createTask(task, );
+                                           @RequestHeader("Authorization") String jwt) throws Exception {
+        UserDto user = userService.getUserProfile(jwt);
+        Task createdTask = taskService.createTask(task, user.getRole());
+
+        return new ResponseEntity<>(createdTask, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id,
+                                           @RequestHeader("Authorization") String jwt) throws Exception {
+        UserDto user = userService.getUserProfile(jwt);
+        Task task = taskService.getTaskById(id);
+
+        return new ResponseEntity<>(task, HttpStatus.OK);
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<List<Task>> getAssignedUsersTask(
+            @RequestParam(required = false) TaskStatus status,
+            @RequestHeader("Authorization") String jwt) throws Exception {
+        UserDto user = userService.getUserProfile(jwt);
+        List<Task> tasks = taskService.assignedUsersTask(user.getId(), status);
+
+        return new ResponseEntity<>(tasks, HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Task>> getAllTask(
+            @RequestParam(required = false) TaskStatus status,
+            @RequestHeader("Authorization") String jwt) throws Exception {
+        UserDto user = userService.getUserProfile(jwt);
+        List<Task> tasks = taskService.getAllTask(status);
+
+        return new ResponseEntity<>(tasks, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}/user/{userid}/assigned")
+    public ResponseEntity<Task> assignedTaskToUser(
+            @PathVariable Long id,
+            @PathVariable Long userid,
+            @RequestHeader("Authorization") String jwt) throws Exception {
+        UserDto user = userService.getUserProfile(jwt);
+        Task tasks = taskService.assignedToUser(userid, id);
+
+        return new ResponseEntity<>(tasks, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> updateTask(
+            @PathVariable Long id,
+            @RequestBody Task req,
+            @RequestHeader("Authorization") String jwt) throws Exception {
+        UserDto user = userService.getUserProfile(jwt);
+        Task tasks = taskService.updateTask(id, req, user.getId());
+
+        return new ResponseEntity<>(tasks, HttpStatus.OK);
     }
 }
--- Task Service/src/main/java/com/day/service/UserService.java
+++ Task Service/src/main/java/com/day/service/UserService.java
@@ -1,7 +1,13 @@
 package com.day.service;
 
+import com.day.model.UserDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "USER_SERVICE", url = "")
+@FeignClient(name = "USER_SERVICE", url = "http://localhost:8080")
 public interface UserService {
+    @GetMapping("/api/users/profile")
+    public UserDto getUserProfile(@RequestHeader("Authorization") String jwt);
+
 }
