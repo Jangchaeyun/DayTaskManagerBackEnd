aa4e2eb2b2812fda3aaa3fd483cfed0441f01522
Jangchaeyun <zzangsally@naver.com>
2024-07-01 22:29:21 +0900

    0701 application.yaml

--- gatway/src/main/java/com/day/gatway/GatwayApplication.java
+++ gatway/src/main/java/com/day/gatway/GatwayApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Eu
 public class GatwayApplication {
 
     public static void main(String[] args) {
