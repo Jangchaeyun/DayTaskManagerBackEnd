62205742102bf8fe9fca0498ce616691aaa14457
Jangchaeyun <zzangsally@naver.com>
2024-07-02 23:36:12 +0900

    0702 Gateway

--- gatway/src/main/java/com/day/gatway/GatwayApplication.java
+++ gatway/src/main/java/com/day/gatway/GatwayApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Eu
 public class GatwayApplication {
 
     public static void main(String[] args) {
